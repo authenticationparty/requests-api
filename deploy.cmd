154606c84918ac0336dd17b4f199d25603131134
tinopai <eltinopa@icloud.com>
2022-01-18 00:04:37 -0300

    Initial commit

---
+++ deploy.cmd
@@ -0,0 +1,19 @@
+@echo off
+setlocal enabledelayedexpansion 
+echo =[ Zipping File ]=
+7z a lambdafunc.zip ./src/*
+
+set names[0]=FetchURL
+set regions[0]=us-west-1
+
+set names[1]=FetchURL-EUW
+set regions[1]=eu-west-2
+
+set names[1]=FetchURL-SAE
+set regions[1]=sa-east-1
+
+(for /l %%i in (0, 1, 1) do ( 
+    call echo =[ Deploying to !names[%%i]! !regions[%%i]! ]=
+    aws configure set region !regions[%%i]!
+    aws lambda update-function-code --function-name !names[%%i]! --zip-file fileb://lambdafunc.zip >nul
+))
---
+++ src/index.js
@@ -0,0 +1,53 @@
+const fetch = (...args) => import('node-fetch')
+              .then(({default: fetch}) => fetch(...args));
+
+
+exports.handler = async (event) => {
+    // CORS
+    if (event.method == 'OPTIONS') {
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': 'https://requests.auth.party',
+                'Access-Control-Allow-Methods': 'POST',
+                'Access-Control-Allow-Headers': 'Content-Type',
+            }
+        }
+    }
+    
+    // TODO implement
+    if (!event.body) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                success: false,
+                message: 'Missing body',
+            }),
+        }
+    }
+
+    const body = JSON.parse(event.body);
+
+    const rdata = await fetch(body.url || `https://auth.party/example`, {
+        method: body.method || 'GET',
+        headers: body.headers || {
+            'User-Agent': 'AuthPartyProxyClient'
+        },
+        body: body.body || null,
+    })
+    
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            event,
+            success: true,
+            data: {
+                status: rdata.status,
+                headers: rdata.headers,
+                body: await rdata.text(),
+                redirect: rdata.redirected,
+                size: rdata.bodyUsed ? rdata.bodyUsed.size : 0,
+            }
+        } || {}),
+    };
+};
