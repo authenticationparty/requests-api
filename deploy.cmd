44264dc325e1fe3258f67e6e318c7f8f59574047
tinopai <eltinopa@icloud.com>
2022-01-18 00:09:39 -0300

    fix: SAE overriding EUW

--- deploy.cmd
+++ deploy.cmd
@@ -9,10 +9,10 @@ set regions[0]=us-west-1
 set names[1]=FetchURL-EUW
 set regions[1]=eu-west-2
 
-set names[1]=FetchURL-SAE
-set regions[1]=sa-east-1
+set names[2]=FetchURL-SAE
+set regions[2]=sa-east-1
 
-(for /l %%i in (0, 1, 1) do ( 
+(for /l %%i in (0, 1, 2) do ( 
     call echo =[ Deploying to !names[%%i]! !regions[%%i]! ]=
     aws configure set region !regions[%%i]!
     aws lambda update-function-code --function-name !names[%%i]! --zip-file fileb://lambdafunc.zip >nul
